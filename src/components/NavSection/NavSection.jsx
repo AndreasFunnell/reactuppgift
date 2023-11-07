d109de9db4336f02fb09a179934a683fe52fcd98
Andreas <andreasfunnell@gmail.com>
2023-11-07 14:38:23 +0100

    la till bilder till header

--- src/components/NavSection/NavSection.jsx
+++ src/components/NavSection/NavSection.jsx
@@ -1,44 +1,45 @@
 import React from 'react'
 import './NavSection.css'
+import logotype from '../../assets/images/Logo.svg'
 
 const NavSection = () => {
   return (
-    <section class="nav-section">
-        <div class="container">
+    <section className="nav-section">
+        <div className="container">
 
-            <div class="logotype"> 
+            <div className="logotype"> 
                 <a href="index.html">
-                    <img src="images/Logo.svg" alt="Logo"/>
+                    <img className ="logotype" src={logotype} alt="Logo"/>
                 </a>
             </div>
-            <div class="contactinformation-bar">
-                <div class="content-box">
-                    <i class="fa-sharp fa-regular fa-phone-volume"></i>
+            <div className="contactinformation-bar">
+                <div className="content-box">
+                    <i className="fa-sharp fa-regular fa-phone-volume"></i>
                     +46 (8) 121 470 50
                 </div>
-                <div class="content-box">
-                    <i class="fa-regular fa-envelope"></i>
+                <div className="content-box">
+                    <i className="fa-regular fa-envelope"></i>
                     info@crito.com
                 </div>
-                <div class="content-box last">
-                    <i class="fa-sharp fa-regular fa-location-dot"></i>
+                <div className="content-box last">
+                    <i className="fa-sharp fa-regular fa-location-dot"></i>
                     Sveavägen 31, 111 34 STOCKHOLM
                 </div>
             </div>
-            <div class="socialmedia-bar">
-                <a href="https://facebook.com" target="_blank"><i class="fa-brands fa-facebook"></i></a>
-                <a href="https://twitter.com" target="_blank>"><i class="fa-brands fa-x-twitter"></i></a>
-                <a href="https://instagram.com" target="_blank>"><i class="fa-brands fa-instagram"></i></a>
-                <a href="https://linkedin.com/" target="_blank>"><i class="fa-brands fa-linkedin"></i></a>  
+            <div className="socialmedia-bar">
+                <a href="https://facebook.com" target="_blank"><i className="fa-brands fa-facebook"></i></a>
+                <a href="https://twitter.com" target="_blank>"><i className="fa-brands fa-x-twitter"></i></a>
+                <a href="https://instagram.com" target="_blank>"><i className="fa-brands fa-instagram"></i></a>
+                <a href="https://linkedin.com/" target="_blank>"><i className="fa-brands fa-linkedin"></i></a>  
             </div>
-            <div class="menu">
+            <div className="menu">
                 <nav>
-                    <a class="active" href="index.html">Home</a>
+                    <a className="active" href="index.html">Home</a>
                     <a href="services.html">Service</a>
                     <a href="news.html">News</a>
                     <a href="contacts.html">Contact</a>
                 </nav>
-                <a class="btn-yellow btn-login" href="login.html">Login <i class="fa-regular fa-arrow-up-right"></i></a>
+                <a className="btn-yellow btn-login" href="login.html">Login <i className="fa-regular fa-arrow-up-right"></i></a>
             </div>
         </div>
     </section>
--- src/components/ShowcaseSection/ShowcaseSection.jsx
+++ src/components/ShowcaseSection/ShowcaseSection.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import './ShowcaseSection.css'
+import ShowcaseImage from '../../assets/images/Showcase-image.png'
 
 const ShowcaseSection = () => {
   return (
-    <section class="showcase-section">
-        <img class ="background-lines-showcase" src="images/Squiggle1.svg" alt=""/>
-        <div class="container">
-            <div class="content">
+    <section className="showcase-section">
+        <img className ="background-lines-showcase" src="images/Squiggle1.svg" alt=""/>
+        <div className="container">
+            <div className="content">
                 <h1>We Provide the Best Business Solutions</h1>
                 <p>Establish your vision, value proposition and turn them into testable prototypes.</p>
-                <a class="btn-yellow" href="consulting.html">Get Consulting <i class="fa-regular fa-arrow-up-right"></i></a>
-                <a class="btn-transparent" href="services.html">Learn More <i class="fa-regular fa-arrow-up-right"></i></a>
+                <a className="btn-yellow" href="consulting.html">Get Consulting <i className="fa-regular fa-arrow-up-right"></i></a>
+                <a className="btn-transparent" href="services.html">Learn More <i className="fa-regular fa-arrow-up-right"></i></a>
             </div>
-            <img src="images/Showcase-Image.png" alt="showcase image of a man in a suit with a tablet"/>
+            <img  className="showcase-image" src={ShowcaseImage} alt="showcase image of a man in a suit with a tablet"/>
         </div>
     </section>
   )
