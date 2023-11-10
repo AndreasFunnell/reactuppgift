f4789fcaa02158065b82947ef070a890e3622d32
Andreas <andreasfunnell@gmail.com>
2023-11-10 18:00:12 +0100

    this guy

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
 import './index.css'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className="wrapper">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
   </React.StrictMode>,
 )
