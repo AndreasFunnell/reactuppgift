bb4443226cbc2557e1db3c1c1ea1d6ce50d094cb
Andreas <andreasfunnell@gmail.com>
2023-11-13 20:06:52 +0100

    la till enkel validering till ContactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,58 +4,101 @@ import { useState } from 'react'
 
 const ContactForm = () => {
     const [name, setName] = useState('')
+    const [nameError, setNameError] = useState('')
     const [email, setEmail] = useState('')
+    const [emailError, setEmailError] = useState('')
     const [message, setMessage] = useState('')
+    const [messageError, setMessageError] = useState('')
+    const [errorMessage, setErrorMessage] = useState ('')
+
+    const handleChange = (e) => {
+        console.log(e.target.type)
+
+        switch (e.target.name) {
+            case 'name':
+                setName(e.target.value)
+                setNameError (validateLength(e.target.value, 1))
+                break
+            case 'email':
+                setEmail(e.target.value)
+                setEmailError (validateLength(e.target.value, 3))
+                break
+            case 'message':
+                setMessage(e.target.value)
+                setMessageError (validateLength(e.target.value, 5))
+                break
+
+        }
+    }
+
+    const validateLength = (value, minLength = 1) => {
+        if (value.length < minLength)
+            return true
+        return false
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log('handling submit')
+        setErrorMessage('')
 
-        const user ={name, email, message}
-        const json = JSON.stringify(user)
-
-        const result = await fetch('https://win23-assignment.azurewebsites.net/api/contactform', {
-            method: 'post',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: json
-        })
-
-        switch (result.status) {
-            case 200:
-                console.log ('meddelandet har skickats')
-                break
-            case 400:
-                console.log (`något har gått fel. felmeddelandet är ${await result.text()}`)
-                break
+        for(let element of e.target) {
+            switch (element.name) {
+                case 'name':
+                    setName(element.value)
+                    setNameError (validateLength(element.value, 1))
+                    break
+                case 'email':
+                    setEmail(element.value)
+                    setEmailError (validateLength(element.value, 3))
+                    break
+                case 'message':
+                    setMessage(element.value)
+                    setMessageError (validateLength(element.value, 5))
+                    break
+            }
         }
 
+        if (!nameError && !emailError && !messageError){
+            const result = await fetch('https://win23-assignment.azurewebsites.net/api/contactform',{
+                method: 'post',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name, email, message
+                })
+            })
 
+            if (result.status === 200)
+                alert('Thank you for your message.')
+            else
+                setErrorMessage('Something went wrong.')
+
+        }
+        
     }
-
-  
   
     return (
     <section className="contact-form">
         <h2>Leave us a message for any information</h2>
+        <p className="errorMessage">{errorMessage}</p>
         <form onSubmit={handleSubmit} noValidate>
         <div className="form-group">
-            <label for="name">Name</label>
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+            <label className={`${nameError ? 'error': ''}`}>{`Name ${nameError ? ' is required': ''}`}</label>
+            <input type="text" name="name" value={name} onChange={(e) => handleChange(e)} />
         </div>
 
         <div className="form-group">
-            <label for="email">Email</label>
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <label className={`${emailError ? 'error': ''}`}>{`Email ${emailError ? ' is required': ''}`}</label>
+            <input type="email" name="email" value={email} onChange={(e) => handleChange(e)} />
         </div>
 
         <div className="form-group message-group">
-            <label for="message">Your Message</label>
-            <input type="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+            <label className={`${messageError ? 'error': ''}`}>{`Message ${messageError ? ' is required': ''}`}</label>
+            <input type="message" name="message" value={message} onChange={(e) => handleChange(e)} />
         </div>
 
-        <button type="submit">Submit</button>
+        <button type="submit">Send Message</button>
         </form>
     </section>
   )
